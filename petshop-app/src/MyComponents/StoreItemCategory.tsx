ff4ce8ccc00a3e283e712da2e7c8120796805938
Bhavanawankhede <97490023+Bhavanawankhede@users.noreply.github.com>
2023-03-17 19:06:31 +0530

    Update StoreItemCategory.tsx

--- petshop-app/src/MyComponents/StoreItemCategory.tsx
+++ petshop-app/src/MyComponents/StoreItemCategory.tsx
@@ -67,7 +67,7 @@ export function StoreItemCategory({
       <Card>
         <CardMedia
           component="img"
-          image=https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg'
+          image='https://cdn.britannica.com/60/8160-050-08CCEABC/German-shepherd.jpg'
           style={Style}
         />
         <CardContent>
